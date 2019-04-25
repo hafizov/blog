e005c33ab82ede34efabe2331cff8f9365c8e474
Abdullo <hafizov.abdullo@live.com>
2019-04-25 22:27:21 +0500

    grid spacing and card tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,12 @@
         <nav class="bg-white">
             <div class="container mx-auto">
                 <div class="flex justify-between items-center py-2">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+
+                    <h1>
+                        <a class="text-black no-underline" href="{{ url('/') }}">
+                            {{ config('app.name', 'Laravel') }}
+                        </a>
+                    </h1>
 
                     <div>
                         <!-- Right Side Of Navbar -->
